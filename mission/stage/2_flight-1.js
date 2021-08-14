c68f0a11ebbc8afaaf23142d742a521d1db97f98
Scott Elcomb <psema4@gmail.com>
2021-08-14 18:58:41 +0100

    disable kybd input indicator

--- mission/stage/2_flight-1.js
+++ mission/stage/2_flight-1.js
@@ -18,6 +18,7 @@ class Flight1 extends GameEngine {
             fontsize: 32
         })
 
+        /*
         if (u||d||l||r) {
             this.drawText('*', {
                 center: {
@@ -32,6 +33,7 @@ class Flight1 extends GameEngine {
 
             // console.debug(`udlr: ${u||'-'}${d||'-'}${l||'-'}${r||'-'}`)
         }
+        */
     }
 
     handleClick(x=-1, y=-1) {
